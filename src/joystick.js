3cf075175e668e334b6b9b2060ca7846174b36f8
Alexander Haggart <alex.haggart@gmail.com>
2018-05-06 12:21:28 -0700

    joystick

--- src/joystick.js
+++ src/joystick.js
@@ -1,4 +1,41 @@
-var computeAddress = "127.0.0.1:8002";
+var computeAddress = window.location.hostname + ":8002";
+// var computeAddress = "192.168.4.1:8002";
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
 
 function httpRequest(addr,method,data,callbacks){
     const shouldBeAsync = true;
@@ -20,7 +57,7 @@ function httpRequest(addr,method,data,callbacks){
 var options = {
     zone:document.getElementById("joystick-container"),
     mode:"static",
-    size:100,
+    size:500,
 }
 const manager = require('nipplejs').create(options);
 const j0 = manager.get(0);
@@ -38,28 +75,35 @@ const joy_msg = {
     buttons:[],
 };
 
-// j0.on("move",(evt,data)=>{
-//     let angle = data.angle.radian;
-//     let mag = data.distance / (options.size/2);
-//     let x = mag*Math.cos(angle);
-//     let y = mag*Math.sin(angle);
-//     joy_msg.axes[0] = x;
-//     joy_msg.axes[1] = y;
-//     joy_msg.updated = true;
-//     // console.log("x: " + x + " y: " + y);
-// });
-
-j0.on("end",(evt,data)=>{
-    joy_msg.axes[0] = 0;
-    joy_msg.axes[1] = 0;
-    joy_msg.updated = true;
-    console.log("released joystick");
+j0.on("move end",(evt,data)=>{
+    if(evt.type === "move"){
+        let angle = data.angle.radian;
+        let mag = data.distance / (options.size/2);
+        let x = mag*Math.cos(angle);
+        let y = mag*Math.sin(angle);
+        joy_msg.axes[0] = x;
+        joy_msg.axes[1] = y;
+        joy_msg.updated = true;
+        console.log("x: " + x + " y: " + y);
+    }else if(evt.type === "end"){
+        joy_msg.axes[0] = 0;
+        joy_msg.axes[1] = 0;
+        joy_msg.updated = true;
+        console.log("released joystick");
+    }
 });
+
+// j0.on("end",(evt,data)=>{
+//     joy_msg.axes[0] = 0;
+//     joy_msg.axes[1] = 0;
+//     joy_msg.updated = true;
+//     console.log("released joystick");
+// });
 
 const pack_joy_msg = function(msg){
     return JSON.stringify({
         action:"joystick-drive",
-        data:JSON.stringify(msg),
+        data:msg,
     });
 };
 
@@ -95,5 +139,9 @@ manager.forEach(function (nipple) {
         y: scroll.y + pos.top
     };
 });
+
+disableScroll();
+
+console.log(window.location.hostname)
 
 // window.setInterval(()=>{},100);
